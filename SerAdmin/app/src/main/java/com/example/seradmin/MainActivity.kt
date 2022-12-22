1355b44eefb44a2a8e626734a00f634e8f78da0e
Stefy_Lissa <stefanialissa7@gmail.com>
2022-12-22 22:57:06 +0100

    App base vacía + Firebase sync
    Proyecto Android sincronizado con Firebase

---
+++ SerAdmin/app/src/main/java/com/example/seradmin/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.seradmin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Analytics Event
+        // Conecta con Analytics y muestra los datos en FireBase.
+        // Tardan unos días en llegar los primeros datos
+        val analytics:FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integración de Firebase completa")
+        analytics.logEvent("InitScreen", bundle)
+    }
+}
