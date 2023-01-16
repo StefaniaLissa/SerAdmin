4025c2bf317ad08ba4efcb80c0471ac01a037221
Stefy_Lissa <stefanialissa7@gmail.com>
2023-01-16 19:52:49 +0100

    Java funciona ?

---
+++ Java/app/src/main/java/com/example/seradmin/Recovery.java
@@ -0,0 +1,14 @@
+package com.example.seradmin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Recovery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recovery);
+    }
+}
