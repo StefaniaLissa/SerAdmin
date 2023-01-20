9291d579b5f8fce3e9768650087605cefb4ac3a2
Stefy_Lissa <stefanialissa7@gmail.com>
2023-01-20 12:05:50 +0100

    Merge de Branchs
    Co-Authored-By: DarioSaiz <96146082+DarioSaiz@users.noreply.github.com>
    Co-Authored-By: Miguefi <114520337+Miguefi@users.noreply.github.com>

---
+++ Java/SerAdmin/app/src/main/java/com/example/seradmin/NuevoCliente.java
@@ -0,0 +1,14 @@
+package com.example.seradmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NuevoCliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_cliente);
+    }
+}
