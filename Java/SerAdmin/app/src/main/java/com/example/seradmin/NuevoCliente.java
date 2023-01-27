5847340f30f44286b817f51926b1cb7c242859c4
Miguefi <martinezmiguel417@gmail.com>
2023-01-27 21:23:41 +0100

    Spinner con placeholder

--- Java/SerAdmin/app/src/main/java/com/example/seradmin/NuevoCliente.java
+++ Java/SerAdmin/app/src/main/java/com/example/seradmin/NuevoCliente.java
@@ -3,9 +3,16 @@ package com.example.seradmin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class NuevoCliente extends AppCompatActivity {
 
@@ -18,10 +25,65 @@ public class NuevoCliente extends AppCompatActivity {
         Resources res = getResources();
         String [] sociedades = res.getStringArray(R.array.sociedades);
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this, R.layout.spinner_texto, sociedades);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this,R.layout.spinner_texto, sociedades) {
+
+            @Override
+            public boolean isEnabled(int position) {
+                if (position == 0) {
+                    // Disable the first item from Spinner
+                    // First item will be use for hint
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView,ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if (tv.getText().toString().equals("Tipo de Sociedad")) {
+                    // Set the hint text color gray
+                    tv.setTextColor(Color.GRAY);
+                    tv.setGravity(Gravity.CENTER);
+                    tv.setTextSize(25);
+                    tv.setPadding(0, 15, 20, 25);
+
+                } else {
+                    tv.setTextSize(18);
+                    tv.setTextColor(Color.BLACK);
+                    tv.setGravity(Gravity.NO_GRAVITY);
+                    tv.setGravity(Gravity.FILL_VERTICAL);
+                    tv.setPadding(55 , 25 , 0 , 25);
+                }
+                return view;
+            }
+
+        };
+
         spinner.setAdapter(adapter);
+
+//        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                String selectedItemText = (String) parent.getItemAtPosition(position);
+//                // If user change the default selection
+//                // First item is disable and it is used for hint
+//                if (position > 0) {
+//                    // Notify the selected item text
+//                    Toast.makeText(getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
+
+
+
+        //spinner.setPrompt("Sociedades");
     }
 }
