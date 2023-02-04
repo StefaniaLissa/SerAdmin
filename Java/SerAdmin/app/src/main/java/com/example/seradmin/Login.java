aa2f1ca1f2a8a1a576bc61be6fe97ae0fbd0f281
Stefy_Lissa <stefanialissa7@gmail.com>
2023-02-04 18:43:27 +0100

    Conexión a FireBase + Analytics

--- Java/SerAdmin/app/src/main/java/com/example/seradmin/Login.java
+++ Java/SerAdmin/app/src/main/java/com/example/seradmin/Login.java
@@ -3,12 +3,32 @@ package com.example.seradmin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class Login extends AppCompatActivity {
+
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        FirebaseAnalytics mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        logo = findViewById(R.id.logo);
+        logo.setOnClickListener(v -> {
+            //Registra evento de click en foto (no funciona)
+            Bundle bundle = new Bundle();
+            bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, logo.getContentDescription().toString());
+            mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+            Toast.makeText(getApplicationContext(), "CLICK", Toast.LENGTH_LONG).show();
+        });
+
+
     }
 }
