29efc95b9c9e55a9c2d0cc3b8da1c038e2ce7b02
Stefy_Lissa <stefanialissa7@gmail.com>
2023-02-09 16:17:50 +0100

    Calendario Updates

--- Java/SerAdmin/app/src/main/java/com/example/seradmin/calendario/EventActivity.java
+++ Java/SerAdmin/app/src/main/java/com/example/seradmin/calendario/EventActivity.java
@@ -2,20 +2,26 @@ package com.example.seradmin.calendario;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatCheckBox;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.seradmin.R;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class EventActivity extends AppCompatActivity {
 
     EditText event_title, event_location, event_description;
     AppCompatCheckBox event_all_day;
     TextView event_start_date, event_start_time, event_end_date, event_end_time, event_repetition;
-    ImageView event_color;
+    ImageView event_color, event_show_on_map;
+    MaterialToolbar event_toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,7 @@ public class EventActivity extends AppCompatActivity {
         setContentView(R.layout.activity_event);
         EditText event_title = (EditText) findViewById(R.id.event_title);
         EditText event_location = (EditText) findViewById(R.id.event_location);
+        ImageView event_show_on_map = findViewById(R.id.event_show_on_map);
         EditText event_description = (EditText) findViewById(R.id.event_description);
         AppCompatCheckBox event_all_day = (AppCompatCheckBox) findViewById(R.id.event_all_day);
         TextView event_start_date = (TextView) findViewById(R.id.event_start_date);
@@ -32,5 +39,34 @@ public class EventActivity extends AppCompatActivity {
         TextView event_repetition = (TextView) findViewById(R.id.event_repetition);
         ImageView event_color = (ImageView) findViewById(R.id.event_color);
 
+//        MaterialToolbar event_toolbar = findViewById(R.id.event_toolbar);
+//        getMenuInflater().inflate(R.menu.menu_event, (Menu) event_toolbar);
+
+        event_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirMapa();
+            }
+        });
+
+        event_show_on_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirMapa();
+            }
+        });
+
+
+
+
+    }
+
+    private void abrirMapa(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        MapaFragment mapaFragment = new MapaFragment();
+        fragmentTransaction.add(mapaFragment, "MapaFragment");
+        fragmentTransaction.commit();
     }
 }
